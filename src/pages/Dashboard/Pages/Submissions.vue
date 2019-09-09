5a1a55333eeb3d1507a53532d709cb445aa98b86
ErdalDomi <erdal.domi@gmail.com>
2019-09-09 19:18:39 +0200

    submissions name ig and status

--- src/pages/Dashboard/Pages/Submissions.vue
+++ src/pages/Dashboard/Pages/Submissions.vue
@@ -5,12 +5,22 @@
 
       <div class="md-layout-item md-medium-size-100 md-size-25">
         <md-card>
+
               <md-card-media>
                 <md-ripple style="margin-left: auto;
     margin-right: auto;">
                   <img src="https://www.teksteshqip.com/img_upz/galeri_full/182/182857.jpg" alt="People" >
                 </md-ripple>
               </md-card-media>
+<br>
+      <md-card-header>
+        <md-card-header-text>
+          <div class="md-title">Erdal Domi</div>
+          <div class="md-subhead">ig: @erdaldomi</div>
+          <div class="md-subhead">Status: <strong>Approved</strong></div>
+        </md-card-header-text>
+                </md-card-header>
+
 
               <md-card-actions>
                 <md-button class="md-icon-button md-success md-wd">
