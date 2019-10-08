72dc330d62f83c48c4224b962d466f8821160d7d
Visar Domi <visardomi4@gmail.com>
2019-10-08 18:36:53 +0200

    artist list

--- src/pages/Dashboard/Pages/Artists.vue
+++ src/pages/Dashboard/Pages/Artists.vue
@@ -10,24 +10,8 @@
 
     <div class="md-layout">
 
-      <div class="md-layout-item md-medium-size-100 md-size-20">
-        <artist-card button-color="success" cardUserImage="./flori.jpg" name="Flori Mumajesi" description="Turbo Folk"> </artist-card>
-      </div>
-
-      <div class="md-layout-item md-medium-size-100 md-size-20">
-        <artist-card button-color="success" cardUserImage="./flori.jpg" name="Flori Mumajesi" description="Turbo Folk"> </artist-card>
-      </div>
-
-      <div class="md-layout-item md-medium-size-100 md-size-20">
-        <artist-card button-color="success" cardUserImage="./flori.jpg" name="Flori Mumajesi" description="Turbo Folk"> </artist-card>
-      </div>
-
-      <div class="md-layout-item md-medium-size-100 md-size-20">
-        <artist-card button-color="success" cardUserImage="./flori.jpg" name="Flori Mumajesi" description="Turbo Folk"> </artist-card>
-      </div>
-
-      <div class="md-layout-item md-medium-size-100 md-size-20">
-        <artist-card button-color="success" cardUserImage="./flori.jpg" name="Flori Mumajesi" description="Turbo Folk"> </artist-card>
+      <div class="md-layout-item md-medium-size-100 md-size-20" v-for="artist of this.artists">
+        <artist-card button-color="success" :artistId="artist.id" :video="artist.video" :cardUserImage="artist.img" :name="artist.name" :description="artist.bio"> </artist-card>
       </div>
     </div>
   </div>
@@ -36,6 +20,8 @@
 <script>
 
 import { ArtistCard } from "@/pages";
+import { LIST_ARTIST } from "@/store/actions.type";
+import { mapGetters } from "vuex";
 export default {
   name: "Artists",
   components: {
@@ -43,13 +29,33 @@ export default {
   },
   data() {
     return {
-
+      artists: []
     };
   },
   methods:{
     addArtist(){
       this.$router.push({ name: "CreateArtist" });
+    },
+    async fetchArtists() {
+      const TableName = "KM2019-Artist";
+      const Limit = "100";
+      const params = {
+        TableName,
+        Limit
+      };
+      await this.$store.dispatch(LIST_ARTIST, params);
+
+      for (let artist of this.getArtists) {
+        this.artists.push(artist);
+      }
+      this.artists.sort((a, b) => b.ordering - a.ordering);
     }
+  },
+  async mounted() {
+    await this.fetchArtists()
+  },
+  computed: {
+    ...mapGetters(["getArtists"])
   }
 };
 </script>
--- src/pages/Dashboard/Pages/Blog.vue
+++ src/pages/Dashboard/Pages/Blog.vue
@@ -1,17 +1,13 @@
 <template>
-<div>
-
+  <div>
     <div class="md-layout">
       <div class="md-layout-item" style="margin-bottom:5rem;">
         <md-button class="md-success" @click="addBlog()">Add new blog post...</md-button>
       </div>
     </div>
 
-
-  <div class="md-layout">
-
-
-<!-- soon replace below div with 
+    <div class="md-layout">
+      <!-- soon replace below div with 
 
 
       <div
@@ -19,85 +15,83 @@
         :key="driver.id"
 
 
- -->
-    <div
-      class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"    v-for="blog in this.blogs" :key="blog.id">
-      <product-card header-animation="true">
-        <img class="img" slot="imageHeader" :src="blog.img" style="height: 20rem;
-    object-fit: cover;" />
-        <md-icon slot="fixed-button">build</md-icon>
-        <template slot="first-button">
-          <md-icon>art_track</md-icon>
-          <md-tooltip md-direction="bottom">View</md-tooltip>
-        </template>
-        <template slot="second-button">
-          <md-icon>edit</md-icon>
-          <md-tooltip md-direction="bottom">Edit</md-tooltip>
-        </template>
-        <template slot="third-button">
-          <md-icon>close</md-icon>
-          <md-tooltip md-direction="bottom">Remove</md-tooltip>
-        </template>
-        <h4 slot="title" class="title">
-          <a href="#pablo">{{blog.title}}</a>
-          <br>
-          <a href="#pablo">{{blog.titleEn}}</a>
-        </h4>
-        <div slot="description" class="card-description">
-          {{blog.body.substring(0, 80)}}
-          <br>
-          {{blog.bodyEn.substring(0, 80)}}
-        </div>
-        <template slot="footer">
-          <div class="price">
-            <h4>{{blog.date}}</h4>
+      -->
+      <div
+        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
+        v-for="blog in this.blogs"
+        :key="blog.id"
+      >
+        <product-card header-animation="true">
+          <img
+            class="img"
+            slot="imageHeader"
+            :src="blog.img"
+            style="height: 20rem; object-fit: cover;"
+          />
+          <md-icon slot="fixed-button">build</md-icon>
+          <template slot="first-button">
+            <md-icon>art_track</md-icon>
+            <md-tooltip md-direction="bottom">View</md-tooltip>
+          </template>
+          <template slot="second-button">
+            <md-icon>edit</md-icon>
+            <md-tooltip md-direction="bottom">Edit</md-tooltip>
+          </template>
+          <template slot="third-button">
+            <md-icon>close</md-icon>
+            <md-tooltip md-direction="bottom">Remove</md-tooltip>
+          </template>
+          <h4 slot="title" class="title">
+            <a href="#pablo">{{blog.title}}</a>
+            <br />
+            <a href="#pablo">{{blog.titleEn}}</a>
+          </h4>
+          <div slot="description" class="card-description">
+            {{blog.body.substring(0, 80)}}
+            <br />
+            {{blog.bodyEn.substring(0, 80)}}
           </div>
-          <div class="stats">
-            <h4 class="category" v-if="blog.isMainHome">
-              <strong>Duket ne home page</strong>
-            </h4>
-          </div>
-        </template>
-      </product-card>
+          <template slot="footer">
+            <div class="price">
+              <h4>{{blog.date}}</h4>
+            </div>
+            <div class="stats">
+              <h4 class="category" v-if="blog.isMainHome">
+                <strong>Duket ne home page</strong>
+              </h4>
+            </div>
+          </template>
+        </product-card>
+      </div>
     </div>
-
-  </div>
-
-
   </div>
 </template>
 
 <script>
-
-
 import { LIST_BLOGS } from "@/store/actions.type";
 import { mapGetters } from "vuex";
 
-
-import {
-  ProductCard,
-} from "@/components";
+import { ProductCard } from "@/components";
 
 export default {
   name: "Blog",
   components: {
-    ProductCard,
+    ProductCard
   },
   data() {
     return {
-      blogs: [],
-
+      blogs: []
     };
   },
-  methods:{
-    addBlog(){
+  methods: {
+    addBlog() {
       this.$router.push({ name: "CreateBlog" });
     },
     goToBlog(blog) {
       // this.$router.push({ name: blog.title });
       this.$router.push({
         name: "SingleBlog",
-        params: { title: blog.title, id: blog.id}
+        params: { title: blog.title, id: blog.id }
       });
     },
     async fetchBlogs() {
@@ -110,16 +104,15 @@ export default {
       await this.$store.dispatch(LIST_BLOGS, params);
 
       for (let blog of this.getBlogs) {
-          this.blogs.push(blog);
+        this.blogs.push(blog);
       }
-      this.blogs.sort((a, b) => b.ordering - a.ordering)
-
+      this.blogs.sort((a, b) => b.ordering - a.ordering);
     }
   },
-    async mounted() {
+  async mounted() {
     await this.fetchBlogs();
   },
-    computed: {
+  computed: {
     ...mapGetters(["getBlogs"])
   }
 };
